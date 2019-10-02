bd279f229c51d2112b61e8d8d49b1403c6edb1bd
Paulo Stanize <paulostanize@gmail.com>
2019-10-02 14:27:16 -0300

    amp eneblead

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Head from '../components/head'
+import { useAmp } from 'next/amp'
 
-export default () => (
-  <>
-    <Head title={'Frontend challenge Homepage'}></Head>
-    <div>Test</div>
-    <div className="test">Test with sass</div>
-  </>
-)
+export const config = { amp: true }
+
+export default () => {
+  const isAmp = useAmp()
+
+  return (
+    <>
+      <Head title={'Frontend challenge Homepage'}></Head>
+      <div>Test</div>
+      <div className="test">Test with sass</div>
+      <div>is amp active? {isAmp ? 'true' : 'false'}</div>
+    </>
+  )
+}
