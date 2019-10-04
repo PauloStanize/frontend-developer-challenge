17aa8e513ffbe9d554a10fe71e19ddf2075611d8
Paulo Stanize <paulostanize@gmail.com>
2019-10-04 17:32:18 -0300

    added custom routing to match page params and fetch

--- components/Catalogue.js
+++ components/Catalogue.js
@@ -1,7 +1,6 @@
 import { colors } from '../styles'
-
-const products = {"products":[{"id":0,"name":"Product Number 0","image":"//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6829307","oldPrice":361,"price":342,"description":"Product long description number 0 just to make more than one like of text.","installments":{"count":10,"value":34.2}},{"id":1,"name":"Product Number 1","image":"//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6506376","oldPrice":145,"price":119,"description":"Product long description number 1 just to make more than one like of text.","installments":{"count":10,"value":11.9}},{"id":2,"name":"Product Number 2","image":"//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399","oldPrice":169,"price":147,"description":"Product long description number 2 just to make more than one like of text.","installments":{"count":10,"value":14.7}},{"id":3,"name":"Product Number 3","image":"//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399","oldPrice":170,"price":141,"description":"Product long description number 3 just to make more than one like of text.","installments":{"count":10,"value":14.1}},{"id":4,"name":"Product Number 4","image":"//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538","oldPrice":401,"price":376,"description":"Product long description number 4 just to make more than one like of text.","installments":{"count":10,"value":37.6}},{"id":5,"name":"Product Number 5","image":"//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6624363","oldPrice":138,"price":125,"description":"Product long description number 5 just to make more than one like of text.","installments":{"count":10,"value":12.5}},{"id":6,"name":"Product Number 6","image":"//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6124117","oldPrice":271,"price":244,"description":"Product long description number 6 just to make more than one like of text.","installments":{"count":10,"value":24.4}},{"id":7,"name":"Product Number 7","image":"//imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6875461","oldPrice":411,"price":381,"description":"Product long description number 7 just to make more than one like of text.","installments":{"count":10,"value":38.1}}],"nextPage":"frontend-intern-challenge-api.iurykrieger.now.sh/products?page=2"}
-
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const ProductCard = ({ id, name, image, oldPrice, price, description, installments: { count, value } = {} }) => {
   const imageHeight = '150'
@@ -14,7 +13,7 @@ const ProductCard = ({ id, name, image, oldPrice, price, description, installmen
       <div className="name">{name}</div>
       <p className="description">{description}</p>
       <div className="old-price">{`De: R$${oldPrice}`}</div>
-      <div className="price">{`Por: R$${oldPrice}`}</div>
+      <div className="price">{`Por: R$${price}`}</div>
       <div className="installments">{`ou ${count}x de R$${value}`}</div>
       <button>
         Comprar
@@ -72,19 +71,26 @@ const ProductCard = ({ id, name, image, oldPrice, price, description, installmen
   )
 }
 
-export default (params) => (
-  <section>
-    <h4>Sua seleção especial</h4>
+export default ({ products = [] }) => {
+  const router = useRouter()
+  const { page: currentPage } = router.query
 
-    <div className="catalogue-list">
-      {products.products.map((product, key) => <ProductCard key={key} {...product} /> )}
-    </div>
+  return (
+    <section>
+      <h4>Sua seleção especial</h4>
 
-    <button className="load-more-button">
-      Ainda mais produtos aqui!
-    </button>
+      <div className="catalogue-list">
+        {products.map((product, key) => <ProductCard key={key} {...product} />)}
+      </div>
 
-    <style jsx>{`
+      <Link href={`/${+currentPage + 1}`}>
+        <button className="load-more-button">
+          Ainda mais produtos aqui!
+        </button>
+      </Link>
+
+
+      <style jsx>{`
       section {
         display: flex;
         align-items: center;
@@ -113,5 +119,6 @@ export default (params) => (
         width: 260px;
       }
     `}</style>
-  </section>
-)
+    </section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
+// import Head from '../components/head'
+// import Header from '../components/Header'
+// import Catalogue from '../components/Catalogue'
+// import ShareForm from '../components/ShareForm'
+// import Footer from '../components/Footer'
+
+// // export const config = { amp: true }
+
+// export default () => {
+//   return (
+//     <>
+//       <Head title={'Frontend challenge Homepage'}></Head>
+//       <Header />
+//       <Catalogue />
+//       <ShareForm />
+//       <Footer />
+//     </>
+//   )
+// }
+
+import Axios from 'axios'
 import Head from '../components/head'
 import Header from '../components/Header'
 import Catalogue from '../components/Catalogue'
@@ -6,14 +27,23 @@ import Footer from '../components/Footer'
 
 // export const config = { amp: true }
 
-export default () => {
+const IndexPage = ({ products }) => {
   return (
     <>
       <Head title={'Frontend challenge Homepage'}></Head>
       <Header />
-      <Catalogue />
+      <Catalogue products={products} />
       <ShareForm />
       <Footer />
     </>
   )
 }
+
+IndexPage.getInitialProps = async ({ query: { page } }) => {
+  const response = await Axios.get(`https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${page}`)
+  const { products } = response.data
+
+  return { products }
+}
+
+export default IndexPage
